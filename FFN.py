515722bc863b632b00ecb932c362c9070966d8a6
josecarl0s1 <72897182+josecarl0s1@users.noreply.github.com>
2023-11-29 19:32:01 -0600

    FFNFirstDraft
    Co-Authored-By: Julio César  Gómez González <56979850+JulioGmz89@users.noreply.github.com>
    Co-Authored-By: Carlos Jiménez <93278499+CharlyDreem@users.noreply.github.com>
    Co-Authored-By: Alfonso Marcel Gómez <69647198+Naexwk@users.noreply.github.com>
    Co-Authored-By: Iván Pérez <107524892+IvanPGt@users.noreply.github.com>

---
+++ FFN.py
@@ -0,0 +1,37 @@
+import json
+import numpy as np
+import tensorflow as tf
+
+def train_and_save_model(training_data_json, saved_model_path):
+    data = training_data_json
+
+    X_train = np.array([entry['features'] for entry in data['data']])
+    y_train = np.array([entry['label'] for entry in data['data']])
+
+    X_test = np.random.rand(10, 2)
+    y_test = np.random.randint(2, size=(10, 1))
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(8, activation='relu', input_shape=(2,)),
+        tf.keras.layers.Dense(8, activation='relu'),
+        tf.keras.layers.Dense(8, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    model.fit(X_train, y_train, epochs=50, batch_size=8, validation_data=(X_test, y_test))
+
+    loss, accuracy = model.evaluate(X_test, y_test)
+    print(f"Test Loss: {loss:.4f}")
+    print(f"Test Accuracy: {accuracy * 100:.2f}%")
+
+    model.save(saved_model_path)
+    print(f"Model saved at {saved_model_path}")
+
+    predictions = model.predict(X_test)
+
+    print("Predictions:")
+    print(predictions)
+    
+
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+from FFN import train_and_save_model
 
 app = Flask(__name__)
 
@@ -12,8 +13,9 @@ def test():
         return jsonify({"response": "Get Request Called"})
     elif request.method == "POST":
         req_Json = request.json
-        name = req_Json['name']
-        return jsonify({"response": "Hi " + name})
+        #name = req_Json['name']
+        train_and_save_model(req_Json, 'saved_model')
+        return jsonify({"response": "Hi"})
 
 if __name__ == '__main__':
     app.run(debug=True)
